d4dd3e9f5355f026e2c764e1deb24ed1f950fcea
Ashok Vennapusa <26950665+ashokreddy-v7@users.noreply.github.com>
2020-01-17 17:19:40 -0800

    snowflake.py working on vpn

--- src_python/snowflake.py
+++ src_python/snowflake.py
@@ -4,5 +4,18 @@ import sys
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from config import config
 
-con=snowflake.connector.connect(user=config.snowflakeUser,password=config.snowflakePwd,account=config.snowflakeAccount,region=)
-print(con)
+# Gets the version
+ctx = snowflake.connector.connect(
+    user=config.snowflakeUser,
+    password=config.snowflakePwd,
+    account=config.snowflakeAccount,
+    region=config.snowflakeRegion
+    )
+cs = ctx.cursor()
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
